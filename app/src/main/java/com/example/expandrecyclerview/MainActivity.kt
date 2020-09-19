2e3773fb5e268d9a68a77f4ac62d900ce8409352
annh2003 <hungan1988@gmail.com>
2020-09-19 17:52:29 +0700

    Update use Preferences DataStore and Proto DataStore

--- app/src/main/java/com/example/expandrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/expandrecyclerview/MainActivity.kt
@@ -1,10 +1,35 @@
 package com.example.expandrecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
 import androidx.databinding.DataBindingUtil
+import androidx.datastore.CorruptionException
+import androidx.datastore.DataStore
+import androidx.datastore.Serializer
+import androidx.datastore.createDataStore
+import androidx.datastore.preferences.Preferences
+import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.preferencesKey
+import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
 import com.example.expandrecyclerview.databinding.ActivityMainBinding
 import com.example.expandrecyclerview.model.Person
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.*
+import java.util.concurrent.Executors
 
 //https://blog.untitledkingdom.com/refactoring-recyclerview-adapter-to-data-binding-5631f239095f
 //https://medium.com/better-programming/recyclerview-expanded-1c1be424282c
@@ -14,12 +39,145 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: ExpendableRecyclerViewAdapter
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+
+    // declare value for working with datastore
+    lateinit var dataStore: DataStore<Preferences>
+
+    //create key for save data type
+    val PREF_UUID = preferencesKey<String>(name = "uuid")
+
+    // declare value for receive data
+    lateinit var uuid: Flow<String>
+
+    //create proto data store
+    lateinit var protoDataStore: DataStore<TestModel>
+    lateinit var token: Flow<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //create preference data store
+        dataStore = this.createDataStore(name = "MyPreferenceDataStore")
+
+        //create proto data store
+        protoDataStore = this.createDataStore(
+            fileName = "MyProtoDataStore.proto",
+            serializer = MyProtoDataStoreSerializer
+        )
+
+        readUUID()
+        CoroutineScope(Dispatchers.Main).launch {
+            uuid.collect { value ->
+                if (value.isEmpty()) {
+                    val newUUID = UUID.randomUUID().toString()
+                    showToast("Not have UUID saved. Create new UUID ${newUUID} and save to data store")
+                    delay(5000)
+                    saveUUID(newUUID)
+
+                } else {
+                    showToast("Read UUID from preference data store ${value}")
+                }
+            }
+        }
+
+        readToken()
+        CoroutineScope(Dispatchers.Main).launch {
+            token.collect { value ->
+                if (value.isEmpty()) {
+                    val newToken = "Your token ABCDEF"
+                    showToast("Not have token saved. Create new token ${newToken} and save to proto data store")
+                    delay(5000)
+                    saveToken(newToken)
+
+                } else {
+                    showToast("Read the token from proto data store ${value}")
+                }
+            }
+        }
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        showListPerson()
+
+//        checkAuthentication()
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+    }
+
+    private fun checkAuthentication() {
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                showAuthentication()
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                showToast("Biometric features no support with current hardware.")
+            //Use password authentication in this case
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                showToast("Biometric features are currently unavailable.")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                showToast("Please associate a biometric credential with your account.")
+            else ->
+                showToast("An unknown error occurred. Please check your Biometric settings")
+        }
+    }
+
+    private fun showToast(s: String) {
+        this.runOnUiThread {
+            val toast = Toast.makeText(this, s, Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.show()
+        }
+    }
+
+    private fun showAuthentication() {
+        val executor = Executors.newSingleThreadExecutor()
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence
+                ) {
+                    super.onAuthenticationError(errorCode, errString)
+                    runOnUiThread {
+                        showToast("Authentication error: $errString")
+                    }
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    runOnUiThread {
+                        showToast("Authentication failed")
+                    }
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {// 2
+                    super.onAuthenticationSucceeded(result)
+
+                    runOnUiThread {
+                        showToast("Authentication succeeded!")
+                        showListPerson()
+                    }
+                }
+            })
+
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for your app")
+            .setSubtitle("Log in using your biometric credential")
+            .setDeviceCredentialAllowed(true) // 4
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun showListPerson() {
         adapter = ExpendableRecyclerViewAdapter(getListPersonFake())
         binding.recyclerPerson.adapter = adapter
         binding.recyclerPerson.itemAnimator = null
@@ -70,4 +228,43 @@ class MainActivity : AppCompatActivity() {
 
         return description
     }
+
+    suspend fun saveUUID(uuid: String) {
+        dataStore.edit {
+            it[PREF_UUID] = uuid
+        }
+    }
+
+    fun readUUID() {
+        uuid = dataStore.data.map {
+            it[PREF_UUID] ?: ""
+        }
+    }
+
+    object MyProtoDataStoreSerializer : Serializer<TestModel> {
+        override fun readFrom(input: InputStream): TestModel {
+            try {
+                return TestModel.parseFrom(input)
+            } catch (exception: InvalidProtocolBufferException) {
+                throw CorruptionException("Cannot read proto.", exception)
+            }
+        }
+
+        override fun writeTo(t: TestModel, output: OutputStream) = t.writeTo(output)
+    }
+
+
+    suspend fun saveToken(token: String) {
+        protoDataStore.updateData {
+            it.toBuilder().setToken(token).build()
+        }
+    }
+
+    fun readToken() {
+        token = protoDataStore.data.map {
+            it.token
+        }
+    }
 }
+
+
